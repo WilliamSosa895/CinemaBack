4653374ead611e8ef740b7e7228828045948e6af
HamburGlar <estebandmb06@gmail.com>
2026-04-29 11:40:18 -0600

    feat(purchase): persist purchases and send confirmation email

--- src/main/java/com/Esteban/cinema/Model/Purchases.java
+++ src/main/java/com/Esteban/cinema/Model/Purchases.java
@@ -1,5 +1,82 @@
 package com.Esteban.cinema.Model;
 
+import jakarta.persistence.*;
+
+import java.sql.Timestamp;
+import java.util.List;
+
+@Entity
+@Table(name = "purchases")
 public class Purchases {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_purchase")
+    private Long idPurchase;
+
+    @Column(name = "date", nullable = false)
+    private Timestamp date;
+
+    @ManyToOne
+    @JoinColumn(name = "id_user", nullable = false)
+    private Users user;
+
+    @ManyToOne
+    @JoinColumn(name = "id_showtime", nullable = false)
+    private Showtimes showtime;
+
+    @Column(name = "total_amount", nullable = false)
+    private Double totalAmount;
+
+    @OneToMany(mappedBy = "purchase")
+    private List<Seats> seats;
+
+    public Long getIdPurchase() {
+        return idPurchase;
+    }
+
+    public void setIdPurchase(Long idPurchase) {
+        this.idPurchase = idPurchase;
+    }
+
+    public Timestamp getDate() {
+        return date;
+    }
+
+    public void setDate(Timestamp date) {
+        this.date = date;
+    }
+
+    public Users getUser() {
+        return user;
+    }
+
+    public void setUser(Users user) {
+        this.user = user;
+    }
+
+    public Showtimes getShowtime() {
+        return showtime;
+    }
+
+    public void setShowtime(Showtimes showtime) {
+        this.showtime = showtime;
+    }
+
+    public Double getTotalAmount() {
+        return totalAmount;
+    }
+
+    public void setTotalAmount(Double totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+
+    public List<Seats> getSeats() {
+        return seats;
+    }
+
+    public void setSeats(List<Seats> seats) {
+        this.seats = seats;
+    }
 }
 
